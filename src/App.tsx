e87aae1f4985559ce04a9bbc871fd14195817380
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-23 18:38:12 +0900

    Solid

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <>
+      <h1>Hello world!!!!</h1>
+      <Comp />
+    </>
+  );
+};
+
+export default App;
