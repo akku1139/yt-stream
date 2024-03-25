74884a91cc4fba43c8ba5e6000c0b705be910305
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-25 22:50:30 +0900

    b

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,22 @@ import PlayerPage from './pages/player';
 import QueuePage from './pages/queue';
 import SearchPage from './pages/search';
 
+import "./main.css";
+
 const App: Component = () => {
   return (<>
-    <div>
-      <input type="radio" id="page-search" name="pages" checked />
-      <label class="page-label" for="page-search">search</label>
+    <input type="radio" id="page-search" name="pages" checked />
+    <label class="page-label" for="page-search">search</label>
 
-      <input type="radio" id="page-queue" name="pages" checked />
-      <label class="page-label" for="page-queue">queue</label>
+    <input type="radio" id="page-queue" name="pages" checked />
+    <label class="page-label" for="page-queue">queue</label>
 
-      <input type="radio" id="page-player" name="pages" checked />
-      <label class="page-label" for="page-player">player</label>
-    </div>
+    <input type="radio" id="page-player" name="pages" checked />
+    <label class="page-label" for="page-player">player</label>
     <div>
-      <SearchPage class="page-content" />
-      <QueuePage class="page-content" />
-      <PlayerPage class="page-content" />
+      <SearchPage class="page-content" id="page-search-content" />
+      <QueuePage class="page-content" id="page-queue-content" />
+      <PlayerPage class="page-content" id="page-player-content" />
     </div>
   </>);
 };
