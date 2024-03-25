3e6c25099389969cc52b0cb88a328a8430760ac4
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-25 23:16:39 +0900

    test

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,5 @@
 const Player = () => {
-  return <audio src=""></audio>
+  return <audio src="https://invidious.jing.rocks/latest_version?id=JBTK0Wssvzc&itag=139"></audio>
 };
 
 export default Player;
