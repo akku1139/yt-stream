b801a144b9c8d3e97180adc8a21e613302dac948
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-26 15:07:59 +0900

    g

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,7 +2,7 @@ import { nowVideo, nowVideoID } from "../queue.ts";
 
 const Player = () => {
   return  <>
-    <img src={nowVideo().thumbnailURL} />
+    <img src={nowVideo.thumbnailURL} />
     <audio controls src={`https://invidious.jing.rocks/latest_version?id=${nowVideoID()}&itag=139`}></audio>
   </>
 };
