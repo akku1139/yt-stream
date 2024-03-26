184451dde4153ca0a35e750a978b355ad4de3cc1
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-26 17:04:20 +0900

    hack

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -3,23 +3,25 @@ export class YouTubeVideo {
   readonly thumbnailURL: String;
   readonly title: String;
 
-  async constructor(id: String) {
+  constructor(id: String) {
     this.id = id;
     this.thumbnailURL = `https://i.ytimg.com/vi/${id}/sddefault.jpg`;
 
-    const videoInfo = await (await fetch("https://music.youtube.com/youtubei/v1/next?prettyPrint=false", {
-      body: JSON.stringify({
-        context: {
-          client: {
-            clientName: "WEB",
-            clientVersion: "2.20240325.01.00",
+    (async (id: String) => {
+      const videoInfo = await (await fetch("https://music.youtube.com/youtubei/v1/next?prettyPrint=false", {
+        body: JSON.stringify({
+          context: {
+            client: {
+              clientName: "WEB",
+              clientVersion: "2.20240325.01.00",
+            },
           },
-        },
-        videoId: id,
-      }),
-      method: "POST",
-    })).json();
+          videoId: id,
+        }),
+        method: "POST",
+      })).json();
 
-    this.title = videoInfo.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text;
+      this.title = videoInfo.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text;
+    })();
   }
 }
