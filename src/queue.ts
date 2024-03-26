39483c690f145e04d9c73fa4b22b4f51e1521a93
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-27 00:55:27 +0900

    init

--- src/queue.ts
+++ src/queue.ts
@@ -16,7 +16,7 @@ class Queue {
 
   constructor() {
     this.list = [];
-    const [index, setIndex] = createSignal<Number>();
+    const [index, setIndex] = createSignal<Number>(0);
     this.index = index;
     this.setIndex = setIndex;
 
@@ -30,13 +30,14 @@ class Queue {
 
     {
       this.add("JBTK0Wssvzc"); // 初回にundefinedなる暫定措置
-      setVideoID("JBTK0Wssvzc");
+      setVideoID("JBTK0Wssvzc");// 曲追加されたときにPlayerを描画してあげれば解決
       const t = new YouTubeVideo();
       t.id = "JBTK0Wssvzc";
       t.title = "【アコーディオン鬼演奏】コンギョ　攻撃戦だ";
       t.thumbnailURL = "https://i.ytimg.com/vi/JBTK0Wssvzc/sddefault.jpg"
       setVideo(t);
     }
+
     createEffect(() => { (async () => {
       this.setVideo(await YouTubeVideo.new(this.nowVideoID()));
     })() });
