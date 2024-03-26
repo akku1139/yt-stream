fc44405a984f0718e37b6d21442d1d7bd2f54722
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-26 15:12:22 +0900

    ゴミのような実装

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,11 @@
-import { nowVideo, nowVideoID } from "../queue.ts";
+import { YouTubeVideo } from "../lib/youtube.ts";
+import { nowVideoID } from "../queue.ts";
 
 const Player = () => {
+
+
   return  <>
-    <img src={nowVideo.thumbnailURL} />
+    <img src={new YouTubeVideo(nowVideoID()).thumbnailURL} />
     <audio controls src={`https://invidious.jing.rocks/latest_version?id=${nowVideoID()}&itag=139`}></audio>
   </>
 };
--- src/queue.ts
+++ src/queue.ts
@@ -3,10 +3,4 @@ import { YouTubeVideo, YouTubeVideo } from "./lib/youtube";
 
 export const [nowVideoID, setVideoID] = createSignal();
 
-export let nowVideo: YouTubeVideo;
-
-createEffect(() => {
-  nowVideo = new YouTubeVideo(nowVideoID());
-});
-
 let queue: Array<YouTubeVideo>;
