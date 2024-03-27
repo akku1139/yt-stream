fbeef89f4ef0bb49e4d9f91f079bd4a57be5ce63
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-27 15:51:24 +0900

    ?????

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,15 @@ import { YouTubeVideo } from '../lib/youtube';
 import queue from '../queue';
 
 export const AddCard: Component = (p: {video: YouTubeVideo}) => {
-  return <div class="video-card" onClick={(e) => {queue.add(p.video)}}>
+  return <a class="video-card" onClick={(e) => {queue.add(p.video)}}>
     <img src={p.video.thumbnailURL} />
     <div>{p.video.title}</div>
-  </div>;
+  </a>;
 };
 
 export const SwitchCard: Component = (p: {video: YouTubeVideo, index: Number}) => {
-  return <div class="video-card" onClick={(e) => {queue.setIndex(p.index)}}>
+  return <a class="video-card" onClick={(e) => {queue.setIndex(p.index)}}>
     <img src={p.video.thumbnailURL} />
     <div>{p.video.title}</div>
-  </div>;
+  </a>;
 };
